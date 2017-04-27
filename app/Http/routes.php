abdcbcf1d669642773ea0899caf280177f8e5e74
user <Global@mail.ua>
2017-04-27 20:30:04 +0300

    common/errors.blade.php add task

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,25 +1,36 @@
 <?php
 
 use App\Task;
-  use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 
-  /**
-   * Вывести панель с задачами
-   */
-  Route::get('/', function () {
+/**
+ * Вывести панель с задачами
+ */
+Route::get('/', function () {
     return view('tasks');
-  });
+});
 
-  /**
-   * Добавить новую задачу
-   */
-  Route::post('/task', function (Request $request) {
-    //
-  });
+/**
+ * Добавить новую задачу
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+		'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+	return redirect('/')
+			->withInput()
+			->withErrors($validator);
+    }
+    $task=new Task();
+    $task->name=$request->name;
+    $task->save();
+    return redirect('/');
+});
 
-  /**
-   * Удалить задачу
-   */
-  Route::delete('/task/{task}', function (Task $task) {
+/**
+ * Удалить задачу
+ */
+Route::delete('/task/{task}', function (Task $task) {
     //
-  });
+});
--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,3 +1,11 @@
-<div>
-    errors will be here
-</div>
+@if (count($errors) > 0)
+  <!-- Список ошибок формы -->
+  <div class="alert alert-danger">
+    <strong>Упс! Что-то пошло не так!</strong>
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif
